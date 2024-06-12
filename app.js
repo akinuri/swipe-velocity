f5089df40d6c9ac918b9d3e95a3962a5a7a8457e
Noreh AD <akinoreh@gmail.com>
2024-06-12 23:24:17 +0300

    limit movement to viewport

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ let shape = {
     height: qs("#shape").clientHeight,
 };
 
+let bounds = {
+    width : innerWidth,
+    height : innerHeight,
+};
+on(window, "resize", () => {
+    bounds.width = innerWidth;
+    bounds.height = innerHeight;
+});
+
 function onDragStart(event) {
     const offsetX = event.clientX - shape.el.getBoundingClientRect().left;
     const offsetY = event.clientY - shape.el.getBoundingClientRect().top;
     function onMouseMove(event) {
         let x = event.clientX - offsetX;
         let y = event.clientY - offsetY;
+        x = clamp(x, 0, bounds.width - shape.width);
+        y = clamp(y, 0, bounds.height - shape.height);
         shape.el.style.left = `${x}px`;
         shape.el.style.top = `${y}px`;
     }
